e0cb39235feca7a178f002e59b582907723aa682
youdeee <youdee.t.ud@gmail.com>
2019-08-18 13:48:55 +0900

    テーブル以外実装

---
+++ lib/issues/cli.ex
@@ -0,0 +1,57 @@
+defmodule Issues.CLI do
+  @default_count 4
+
+  def run(argv) do
+    argv
+    |> parse_args
+    |> process
+  end
+
+  def parse_args(argv) do
+    parse = OptionParser.parse(argv, switches: [help: :boolean], aliases: [h: :help])
+    case parse do
+      {[help: true], _, _} -> :help
+      {_, [user, project, count], _} -> {user, project, String.to_integer(count)}
+      {_, [user, project], _} -> {user, project, @default_count}
+      _ -> :help
+    end
+  end
+
+  def process(:help) do
+    IO.puts """
+    usage: issues <user> <project> [ count | #{@default_count} ]
+    """
+    System.halt(0)
+  end
+
+  def process({user, project, count}) do
+    Issues.GithubIssues.fetch(user, project)
+    |> decode_response
+    |> convert_to_list_of_maps # なくてもいい気がするから後で実験する
+    |> sort_into_ascending_order
+    |> Enum.take(count)
+    |> print_table_for_columns(["number", "created_at", "title"])
+  end
+
+  def decode_response({:ok, body}), do: body
+  def decode_response({:error, error}) do
+    {_, message} = List.keyfind(error, "message", 0)
+    IO.puts "Error fetching from Github: #{message}"
+    System.halt(2)
+  end
+
+  def convert_to_list_of_maps(list) do
+    list
+    |> Enum.map(&Enum.into(&1, Map.new))
+  end
+
+  def sort_into_ascending_order(list_of_issues) do
+    Enum.sort list_of_issues,
+    fn i1, i2 -> i1["created_at"] <= i2["created_at"] end
+  end
+
+  def print_table_for_columns(list, columns) do
+    for column <- columns, do: IO.puts "#{column},"
+    for obj <- list, column <- columns, do: IO.puts obj[column]
+  end
+end
